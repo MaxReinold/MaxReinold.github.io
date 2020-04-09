ef850e06275e6442555bc25e01de0b9d4a236bdb
StealthSov <50644368+StealthSov@users.noreply.github.com>
2020-04-09 12:24:30 -0400

    Large Redisign
    Added Used Car part upgrades

---
+++ scrapupgrades/script.js
@@ -0,0 +1,23 @@
+function upgrade(){
+  let total =
+    parseInt(document.getElementById('engineLevel').value) +
+    parseInt(document.getElementById('brake').value) +
+    parseInt(document.getElementById('transmission').value) +
+    parseInt(document.getElementById('suspension').value) +
+    parseInt(document.getElementById('wheels').value) +
+    //parseInt(document.getElementById('armor').value) +
+    parseInt(document.getElementById('paint').value);
+  if(document.getElementById("turbo").checked){
+    total += 7500;
+  }
+  if(document.getElementById("xenon").checked){
+    total += 200;
+  }
+  if(document.getElementById("live").checked){
+    total += 4000;
+  }
+  if(document.getElementById('employee').checked){
+    total = total * .7;
+  }
+  document.getElementById('finalPrice').innerHTML = "$" + total + ".00";
+}
--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let onUpgrades = true;
+let onNew = true;
 function swap(){
   if(onUpgrades){
     document.getElementById('display').src = "repairs/index.html";
     document.getElementById('aButton').innerHTML = "Upgrades";
+    document.getElementById('upgradetype').style.visibility = "hidden";
   } else {
     document.getElementById('display').src = "upgrades/index.html";
     document.getElementById('aButton').innerHTML = "Repairs";
+    document.getElementById('upgradetype').style.visibility = "visible";
   }
   onUpgrades = !onUpgrades;
+}
+
+function swapType(){
+  if(onNew){
+    document.getElementById('display').src = "scrapupgrades/index.html";
+    document.getElementById('upgradetype').innerHTML = "New Parts";
+  } else {
+    document.getElementById('display').src = "upgrades/index.html";
+    document.getElementById('upgradetype').innerHTML = "Used / Scrap Parts";
+  }
+  onNew = !onNew;
 }
