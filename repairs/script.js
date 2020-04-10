fb9ef151120924fbc053a44a21328ceed2aaa0ac
StealthSov <50644368+StealthSov@users.noreply.github.com>
2020-04-09 20:49:34 -0400

    Add tow miles calculator

--- repairs/script.js
+++ repairs/script.js
@@ -10,9 +10,12 @@ function repair(){
     if(document.getElementById('window').checked){
       total += 350;
     }
+    let scalar = parseFloat(document.getElementById('priceScale').value);
+    if(parseFloat(document.getElementById('towMiles').value) > 0){
+      total += 1000 + Math.floor(parseFloat((document.getElementById('towMiles').value -1) * 100));
+    }
     if(document.getElementById('employee').checked){
       total = total * .7;
     }
-    let scalar = parseFloat(document.getElementById('priceScale').value);
     document.getElementById('finalPrice').innerHTML = "$" + Math.floor(total * scalar * .01) + ".00";
 }
