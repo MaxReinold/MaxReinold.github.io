d42d58ca347ca2f8bdedbf260168e12dedae18e5
StealthSov <cmaxreinold@gmail.com>
2021-08-26 12:37:06 -0400

    Swapped websites

--- scripts/master.js
+++
@@ -1,9 +0,0 @@
-function popup(mylink, windowname, width, height) 
-{ 
-    if (! window.focus)return true; 
-    var href; 
-    if (typeof(mylink) == 'string') href=mylink; 
-    else href=mylink.href; 
-    window.open(href, windowname, 'top=150,left=150,width='+width+',height='+height+',scrollbars=yes'); 
-    return false; 
-}
